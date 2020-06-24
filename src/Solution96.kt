3b7f65df75c78a98e1b7a1c0c0c149f4079ba578
Kirill Temnenkov <kirill@temnenkov.com>
2020-06-24 13:02:03 +0300

    https://leetcode.com/problems/unique-binary-search-trees/

---
+++ src/Solution96.kt
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/unique-binary-search-trees/
+class Solution96 {
+    fun numTrees(n: Int): Int {
+        val count = IntArray(n + 1)
+        count[0] = 1
+        count[1] = 1
+        for (i in 2..n) {
+            for (j in 0 until i) {
+                count[i] = count[i] + count[j] * count[i - j - 1]
+            }
+        }
+        return count[n]
+    }
+}
